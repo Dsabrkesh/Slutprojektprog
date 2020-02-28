5143479e296cce8c62cc07b8a6093ca02cdf19f5
Dsabrkesh <42805455+Dsabrkesh@users.noreply.github.com>
2020-02-29 00:16:34 +0100

    slutprojekt
    slutprojekt

---
+++ server.js
@@ -0,0 +1,161 @@
+const express = require('express')
+const app = express()
+const { decode } = require("querystring");
+const { updateDb } = require("./myModule");
+const port = 3000
+const myModule = require('./myModule')
+
+  const mongoose = require('mongoose')
+  mongoose.connect("mongodb://localhost:27017/", { useNewUrlParser: true })
+  const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('Connected to Mongoose'))
+
+  const clientDir = __dirname + '\\client\\'
+
+app.get('/', (requset, response) => response.sendFile(clientDir + 'index.html'))
+app.get('/registration.html', (requset, response) => response.sendFile(clientDir + 'registration.html'))
+app.get('/login.html', (requset, response) => response.sendFile(clientDir + 'login.html'))
+app.get('/chattbox.html', (requset, response) => response.sendFile(clientDir + 'chattbox.html'))
+app.get('/registration.html', (requset, response) => response.sendFile(clientDir + 'login.html'))
+app.get('/register.css', (requset, response) => response.sendFile(clientDir + 'registration.html'))
+app.get('/login.css', (requset, response) => response.sendFile(clientDir + 'login.html'))
+app.get('/registration.html', (requset, response) => response.sendFile(clientDir + 'chattbox.html'))
+app.get('/login.html', (requset, response) => response.sendFile(clientDir + 'chattbox.html'))
+app.get('users', (req, res) => {
+    res.json(users)
+})
+const io = require('socket.io')(3000)
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
+app.listen(3000)
